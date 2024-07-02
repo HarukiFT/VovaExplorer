20cb64ac22c6910d4320f03282add67cf0312d7d
Haruki <aratashiharuki@gmail.com>
2024-07-03 00:37:41 +0300

    Provide CardContext

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,5 +1,7 @@
 import Header from "./Header"
 import Styles from '../styles/App.module.scss'
+import CardsHolder from "./CardsHolder"
+import { CardContextProvider } from "../contexts/CardContext"
 
 export default () => {
     return (
@@ -7,6 +9,10 @@ export default () => {
             <Header />
 
             <p className={Styles.siteTitle}>Сервисы для <b>VOVA</b></p>
+            
+            <CardContextProvider>
+                <CardsHolder />
+            </CardContextProvider>
         </div>
     )
 }
