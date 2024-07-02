7cb17412b4ede51e7211711879e318ec96cf0b3b
Haruki <aratashiharuki@gmail.com>
2024-07-03 00:37:17 +0300

    Add placeholder cards

--- frontend/src/components/CardsHolder.js
+++ frontend/src/components/CardsHolder.js
@@ -1,5 +1,17 @@
+import { useState } from 'react'
 import Styles from '../styles/CardsHolder.module.scss'
+import ActiveCard from './ActiveCard'
+import InactiveCard from './InactiveCard'
 
 export default () => {
-    
+    return (
+        <div className={Styles.grid}>
+            <ActiveCard order={1}/>
+            <ActiveCard order={2}/>
+            <ActiveCard order={3}/>
+            <InactiveCard order={4}/>
+            <InactiveCard order={5}/>
+            <InactiveCard order={6}/>
+        </div>
+)
 }
