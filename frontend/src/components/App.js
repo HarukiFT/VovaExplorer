d96ed15e74bed0b4b29ad532ccea3c7965d24007
Haruki <aratashiharuki@gmail.com>
2024-07-02 21:31:53 +0300

    Init

---
+++ frontend/src/components/App.js
@@ -0,0 +1,10 @@
+import Header from "./Header"
+import Styles from '../styles/App.module.scss'
+
+export default () => {
+    return (
+        <div className={Styles.wrapper}>
+            <Header />
+        </div>
+    )
+}
