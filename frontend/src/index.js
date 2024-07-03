1959d1b86230ec43a35215bb86d42770dec0dc3e
Haruki <aratashiharuki@gmail.com>
2024-07-03 05:05:07 +0300

    Include AppContextProvider

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,10 +2,13 @@ import ReactDOM from 'react-dom/client';
 import reportWebVitals from './reportWebVitals';
 import './styles/index.scss'
 import App from './components/App';
+import { AppContextProvider } from './contexts/AppContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <App/>
+    <AppContextProvider>
+      <App/>
+  </AppContextProvider>
 );
 
 reportWebVitals();
