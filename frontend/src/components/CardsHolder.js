8bccaa1d3d422440108e1838ff54286b17812d65
Haruki <aratashiharuki@gmail.com>
2024-07-03 05:04:00 +0300

    Add onClick callback, placeholder modal data

--- frontend/src/components/CardsHolder.js
+++ frontend/src/components/CardsHolder.js
@@ -1,14 +1,20 @@
-import { useState } from 'react'
 import Styles from '../styles/CardsHolder.module.scss'
 import ActiveCard from './ActiveCard'
 import InactiveCard from './InactiveCard'
+import { useAppContext } from '../contexts/AppContext'
 
 export default () => {
+    const { setModalData } = useAppContext()
+
+    const handleClick = (order) => {
+        setModalData({type: 'card'})
+    }
+
     return (
         <div className={Styles.grid}>
-            <ActiveCard order={1}/>
-            <ActiveCard order={2}/>
-            <ActiveCard order={3}/>
+            <ActiveCard onClick={handleClick} order={1}/>
+            <ActiveCard onClick={handleClick} order={2}/>
+            <ActiveCard onClick={handleClick} order={3}/>
             <InactiveCard order={4}/>
             <InactiveCard order={5}/>
             <InactiveCard order={6}/>
