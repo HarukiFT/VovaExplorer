d7deb92a93ffcb220eb39d32d3b3620f52a00798
Haruki <aratashiharuki@gmail.com>
2024-07-03 05:01:53 +0300

    Include app context and modal

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -2,17 +2,25 @@ import Header from "./Header"
 import Styles from '../styles/App.module.scss'
 import CardsHolder from "./CardsHolder"
 import { CardContextProvider } from "../contexts/CardContext"
+import { AppContextProvider, useAppContext } from "../contexts/AppContext"
+import Modal from "./Modal"
 
 export default () => {
-    return (
-        <div className={Styles.wrapper}>
-            <Header />
+    const { modalData } = useAppContext()
 
-            <p className={Styles.siteTitle}>Сервисы для <b>VOVA</b></p>
-            
-            <CardContextProvider>
-                <CardsHolder />
-            </CardContextProvider>
-        </div>
+    return (
+        <>
+            <div className={`${Styles.wrapper} ${modalData ? Styles.blur : ''}`}>
+                <Header />
+
+                <p className={Styles.siteTitle}>Сервисы для <b>VOVA</b></p>
+
+                <CardContextProvider>
+                    <CardsHolder />
+                </CardContextProvider>
+            </div>
+
+            <Modal />
+        </>
     )
 }
