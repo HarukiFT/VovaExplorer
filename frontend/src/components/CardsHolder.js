0cef052a82045f793d716a0e9895eeb7ae4a3fdd
Haruki <aratashiharuki@gmail.com>
2024-07-04 04:53:54 +0300

    Add card render by using api

--- frontend/src/components/CardsHolder.js
+++ frontend/src/components/CardsHolder.js
@@ -2,22 +2,34 @@ import Styles from '../styles/CardsHolder.module.scss'
 import ActiveCard from './ActiveCard'
 import InactiveCard from './InactiveCard'
 import { useAppContext } from '../contexts/AppContext'
+import { useEffect, useState } from 'react'
+import { useCardContext } from '../contexts/CardContext'
 
 export default () => {
     const { setModalData } = useAppContext()
+    const { cardsData } = useCardContext()
 
     const handleClick = (order) => {
-        setModalData({type: 'card'})
+        setModalData({type: 'card', data: cardsData.find(card => card.order == order)})
+    }
+
+    const cards = []
+    const orders = new Array(6).map((_, index) => index + 1)
+
+    for (let i = 0; i < cardsData.length; i++) {
+        const order = cardsData[i].order
+
+        cards.push(<ActiveCard order={order} onClick={handleClick} />)
+        orders.splice(orders.indexOf(order), 1)
+    }
+
+    for (let i = 0; i < orders.length; i++) {
+        cards.push(<InactiveCard order={orders[i]}/>)
     }
 
     return (
         <div className={Styles.grid}>
-            <ActiveCard onClick={handleClick} order={1}/>
-            <ActiveCard onClick={handleClick} order={2}/>
-            <ActiveCard onClick={handleClick} order={3}/>
-            <InactiveCard order={4}/>
-            <InactiveCard order={5}/>
-            <InactiveCard order={6}/>
+            {cards}
         </div>
 )
 }
