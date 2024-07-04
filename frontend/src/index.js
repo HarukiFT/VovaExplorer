27c364eddfaef812508b5a5b42a25fd0cebbdaf5
Haruki <aratashiharuki@gmail.com>
2024-07-04 04:56:11 +0300

    Include ToastContainer

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,11 +3,14 @@ import reportWebVitals from './reportWebVitals';
 import './styles/index.scss'
 import App from './components/App';
 import { AppContextProvider } from './contexts/AppContext';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/ReactToastify.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <AppContextProvider>
       <App/>
+      <ToastContainer/>
   </AppContextProvider>
 );
 
