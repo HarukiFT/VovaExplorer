87cd682a063f40e91d5da76c1b3486940884a5bd
Haruki <aratashiharuki@gmail.com>
2024-07-04 04:54:38 +0300

    Real data from CardContext. Close animation

--- frontend/src/components/Modal/CardModal.js
+++ frontend/src/components/Modal/CardModal.js
@@ -1,8 +1,13 @@
 import { useSpring, animated } from 'react-spring'
 import Styles from '../../styles/Modal/Card.module.scss'
+import { useState } from 'react'
+import { useAppContext } from '../../contexts/AppContext'
 
 
-export default () => {
+export default ({data}) => {
+    const { setModalData } = useAppContext()
+    const [isClosed, setClosed] = useState(false)
+
     const modalSpring = useSpring({
         from: {
             transform: 'scale(50%)',
@@ -10,28 +15,43 @@ export default () => {
         },
 
         to: {
-            transform: 'scale(100%)',
-            opacity: 1
+            transform: isClosed ? `scale(50%)` : `scale(100%)`,
+            opacity: isClosed ? 0 : 1
         },
+
+        config: {
+            tension: isClosed ? 180 : 120, 
+            bounce: isClosed ? 0 : undefined,
+        },
+
+        onRest: () => {
+            if (isClosed) {
+                setModalData(null)
+            }
+        }
     })
+
+    const handleClose = () => {
+        setClosed(true)
+    }
 
     return (
         <animated.div style={modalSpring} className={Styles.wrapper}>
             <div className={Styles.cardName}>
-                <p>Имя карточки</p>
+                <p>{data.name}</p>
             </div>
 
             <div className={Styles.description}>
-                
+                <p>{data.description}</p>
             </div>
 
             <div className={Styles.actions}>
                 <div>
-                    <input type="button" className={Styles.button} value="Репозиторий"/>
+                    <input type="button" className={Styles.button} onClick={handleClose}  value="Вернуться"/>
                 </div>
 
                 <div>
-                    <input type="button" className={Styles.button} value="Перейти"/>
+                    <a type="button" href={data.link} className={Styles.button}>Перейти</a>
                 </div>
             </div>
         </animated.div>
